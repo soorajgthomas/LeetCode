7cf17b72fade4119791a8ca85bf0c65d24053e50
soothomas <v-soothomas@expediagroup.com>
2020-06-04 22:01:32 -0500

    Initial Commit

---
+++ src/com/leetcode/challenge/june/InvertBinaryTree.java
@@ -0,0 +1,59 @@
+package com.leetcode.challenge.june;
+
+/*
+        Day 1 Challenge
+
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+ */
+
+
+
+public class InvertBinaryTree {
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+
+        root.right=left;
+        root.left=right;
+
+        return root;
+    }
+
+}
